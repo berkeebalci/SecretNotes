9ce9883888f9a48f98356d58d1cd3c164717639b
berkebaranbalci <berkebaranbalci@gmail.com>
2025-03-27 22:36:47 +0300

    encrypt feature added.

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
 
 #window
 window = tk.Tk()
 window.title("Secret Notes")
 window.geometry("400x550")
+
+
+
+def encrypt_text():
+    secret_text = entry_secret.get()
+    crypto_master_key = entry_master_key.get()
+    header_text = entry_title.get()
+    encrypted_text = cipher_suite.encrypt(secret_text.encode())
+    encrypted_text = encrypted_text.decode()
+
+    with open("TopSecret.txt" , "w") as file:
+        file.write(header_text + "\n\n")
+        file.write("Encrypted Text:\n")
+        file.write(encrypted_text)
+
+    result_label.config(text="The encrypted text is saved to file.")
+
+
+
 
 #image
 image = Image.open("topsecret.png")
@@ -20,26 +43,29 @@ window.tk_image = tk_image
 enter_your_title = tk.Label(text="Enter your title")
 enter_your_title.pack()
 
-entry_title = tk.Entry(bg="white",width=15)
+entry_title = tk.Entry(bg="white",width=15,fg="black")
 entry_title.pack()
 
 secret = tk.Label(text="Enter your secret")
 secret.pack()
 
-entry_secret = tk.Entry(width=20,bg="white")
+entry_secret = tk.Entry(width=20,bg="white",fg="black")
 entry_secret.pack(pady=10,ipady=70)
 
-master_key = tk.Label(text="Enter master key")
-master_key.pack()
+mk_label = tk.Label(text="Enter master key")
+mk_label.pack()
 
-entry_master_key = tk.Entry(bg="white")
+entry_master_key = tk.Entry(bg="white",fg="black")
 entry_master_key.pack()
 
-encrypt_button = tk.Button(text="Save & Encrypt" ,font="Arial")
+encrypt_button = tk.Button(text="Save & Encrypt" ,font="Arial",command=encrypt_text)
 encrypt_button.pack()
 
 decrypt_button = tk.Button(text="Decrypt")
 decrypt_button.pack()
+
+result_label = tk.Label(text="")
+result_label.pack()
 
 
 
